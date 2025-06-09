5e3ddce9b154e513eb2efaf7cda87077c7664f0e
Aksh Khurana <khuranaaksh08@gmail.com>
2025-06-09 09:39:43 +0530

    openai codes done

--- 02-vector-embeddings/main.py
+++ 02-vector-embeddings/main.py
@@ -1,38 +1,15 @@
-# OPEN AI API KEY
-
-# from dotenv import load_dotenv
-# from openai import OpenAI
-
-# load_dotenv()
-
-# client = OpenAI()
-
-# text = "Dog chases chat"
-
-# response = client.embeddings.create(
-#    model = "text-embedding-3-small",
-#    input = text
-# )
-
-# print(response)
-
-
-
-# GEMINI API KEY
-
 from dotenv import load_dotenv
-import os
-import google.generativeai as genai
+from openai import OpenAI
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+client = OpenAI()
 
 text = "Dog chases chat"
 
-response = genai.embed_content(
-    model="models/embedding-001",
-    content=text,
-    task_type="retrieval_document"  # or "retrieval_query"
+response = client.embeddings.create(
+    model="text-embedding-3-small",
+    input=text
 )
 
-print(response['embedding'])
+print(response)
