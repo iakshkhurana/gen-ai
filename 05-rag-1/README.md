5971e96a034101856ec1df3605271792c814663b
Aksh Khurana <khuranaaksh08@gmail.com>
2025-07-12 17:04:08 +0530

    l-05 completed half

---
+++ 05-rag-1/README.md
@@ -0,0 +1,5 @@
+Langchain is the library jisme bahut saare functions hain, basically like load document,etc.
+
+RAG is retrieval augmented generation
+it is pushing data into llm and then into chats, and the data is like into pdfs toh pdf ko pura nahi dal dete kyunki tokens are limited so they are divided into chunks and pushed.
+sabse pehle data ko load karenge using langchain
---
+++ 05-rag-1/main.py
@@ -0,0 +1,30 @@
+# Data ingestion
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+
+pdf_path = Path(__file__).parent / "all.pdf"
+
+# Loading
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+print("Docs[0]",docs[0])
+
+# Chunking
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+split_docs = text_splitter.split_documents(documents = docs)
+
+# Vector Embeddings
+embedding_model = OpenAIEmbeddings(
+    model = "text-embeddings-3-large"
+)
+
+# Using embedding model create embeddings of split_docs and store in db
+
