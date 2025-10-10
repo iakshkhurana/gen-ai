acb55774a4886592ff59ae1541bd49073db93123
Aksh Khurana <150786734+iakshkhurana@users.noreply.github.com>
2025-10-10 14:56:50 +0530

    graph arch

--- 07-langgraphs/graph.py
+++ 07-langgraphs/graph.py
@@ -1,15 +1,68 @@
 from typing_extensions import TypedDict
+from openai import OpenAI
+# from dotenv import load_dotenv
+from langgraph.graph import StateGraph, START, END
+# load_dotenv()
+# client = OpenAI()
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key="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",
+)
 
 
+# states getting divided into nodes
+#     state
+#    ___|___
+# ___|      |___
+#|              |  
+
+# box ban gya
 class State(TypedDict):
     query: str
     llm_result: str | None
 
+# receives current state
+# uspe llm_call krega
 def chat_bot(state: State):
+
     query = state['query']
-    # llm call
-    result = "Hello, how can i help you?"
+
+    llm_response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "user", "content": query}
+        ]
+    )
+
+    result = llm_response.choices[0].message.content
     state["llm_result"] = result
-    
+
     return state
 
+
+graph_builder = StateGraph(State)
+
+graph_builder.add_node("chat_bot", chat_bot)
+
+graph_builder.add_edge(START, "chat_bot")
+graph_builder.add_edge("chat_bot", END)
+
+graph = graph_builder.compile()
+
+
+def main():
+    user = input("> ")
+
+    # Invoke the graph
+    _state = {
+        "query": user,
+        "llm_result": None
+    }
+
+    graph_result = graph.invoke(_state)
+
+    print("graph_result", graph_result)
+
+
+main()
