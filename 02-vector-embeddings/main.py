ade6fa4b1b40bca293d26b9dca7aaa5b3c09deb8
Aksh Khurana <khuranaaksh08@gmail.com>
2026-03-27 12:04:06 +0530

    pinecone code added

--- 02-vector-embeddings/main.py
+++ 02-vector-embeddings/main.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from pinecone import Pinecone
 
 load_dotenv()
 
 client = OpenAI()
+
+# Vector Embeddings generation
 
 response = client.embeddings.create(
     model="text-embedding-3-small",
@@ -11,4 +14,12 @@ response = client.embeddings.create(
 )
 
 print("Vector Embeddings : ", response)
-print("Length = ",response.data[0])
+print("Length = ", len(response.data[0].embedding))
+
+# Add ON : Pinecone
+
+pc = Pinecone()
+index = pc.Index("idx")
+index.upsert(vectors=[
+    {"id":"node-1","values": response.data[0].embedding, "metadata":{"text":"Node.js is used to write backends."}}
+])
